0d28047fc5b9a05535ca931a91bdffa43376f2d8
Adhinee <103882598+Adhinee@users.noreply.github.com>
2024-12-13 21:26:03 +0530

    Update AddItem.js

--- src/AddItem.js
+++ src/AddItem.js
@@ -9,7 +9,7 @@ const AddItem = ({newItem,setNewItem,handleSubmit}) => {
             autoFocus
             id='addItem'
             type="text"
-            placeholder='Add Item here'
+            placeholder='Add Items in here'
             value={newItem}
             onChange={(e) => setNewItem(e.target.value)}
             required />
